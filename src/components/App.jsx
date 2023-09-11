b73546554cd9cc61fba4b737ebddb6b76127dde4
irapanch <117355580+irapanch@users.noreply.github.com>
2023-09-11 23:26:27 +0300

    add task

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,52 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+// import { nanoid } from 'nanoid';
+// class App extends React.Component {
+//   state = {
+//     contacts: [],
+//     name: '',
+//   };
+
+//   handleNameChange = event => {
+//     this.setState({ name: event.target.value });
+//   };
+
+//   handleFormSubmit = event => {
+//     event.preventDefault();
+
+//     const newContact = {
+//       id: nanoid(),
+//       name: this.state.name,
+//     };
+
+//     this.setState(prevState => ({
+//       contacts: [...prevState.contacts, newContact],
+//       name: '',
+//     }));
+//   };
+
+//   render() {
+//     // Відображення списку контактів
+//     const contactList = this.state.contacts.map(contact => (
+//       <li key={contact.id}>{contact.name}</li>
+//     ));
+
+//     return (
+//       <div>
+//         <form onSubmit={this.handleFormSubmit}>
+//           <input
+//             type="text"
+//             name="name"
+//             pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+//             title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+//             required
+//             value={this.state.name}
+//             onChange={this.handleNameChange}
+//           />
+//           <button type="submit">Add Contact</button>
+//         </form>
+
+//         <ul>{contactList}</ul>
+//       </div>
+//     );
+//   }
+// }
+// export default App;
